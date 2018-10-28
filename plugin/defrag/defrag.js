8f906f5f4d43da48efbc5a13d2e5361633b4eb0d
Salvi <salvi@intre.it>
2018-10-28 19:17:56 +0100

    Custom plugin skips to last fragment on V

--- plugin/defrag/defrag.js
+++ plugin/defrag/defrag.js
@@ -1,71 +1,31 @@
-// Press D to disable all fragments. And again to enable them again.
-var Defrag = ( function( Reveal, global ){
+// Press V to view all fragments in current slide
+const Defrag = (function (Reveal, global) {
 
-    var hasFrag = true;
-
-    var classOn = 'fragment';
-    var classOff = 'no-fragment';
-
-    function defrag() {
-        [...document.getElementsByClassName(classOn)].forEach(y => {
-            y.classList.add(classOff);
-            y.classList.remove(classOn);
-        });
-    }
-    function refrag() {
-        [...document.getElementsByClassName(classOff)].forEach(y => {
-            y.classList.add(classOn);
-            y.classList.remove(classOff);
-        });
+    function skipToLastFragment() {
+        let {h, v} = Reveal.getIndices();
+        Reveal.slide(h, v, +Infinity);
     }
 
-    function toggleFrag() {
-        if (hasFrag) defrag();
-        else refrag();
-        hasFrag = !hasFrag;
-    }
-
-    function installKeyBindings(){
-        var config = Reveal.getConfig();
-        var shortcut = 'D';
-        if( !config.keyboard ){
+    function installKeyBindings() {
+        const config = Reveal.getConfig();
+        const shortcut = 'V';
+        if (!config.keyboard) {
             return;
         }
-        var keyboard = config.keyboard === true ? {} : config.keyboard;
-        keyboard[ shortcut.toUpperCase().charCodeAt( 0 ) ] = toggleFrag;
+        const keyboard = config.keyboard === true ? {} : config.keyboard;
+        keyboard[shortcut.toUpperCase().charCodeAt(0)] = skipToLastFragment;
 
-        Reveal.registerKeyboardShortcut( shortcut, 'Defrag' );
+        Reveal.registerKeyboardShortcut(shortcut, 'Defrag');
         Reveal.configure({
             keyboard: keyboard
         });
     }
 
-    function installDependencyCallback( c ){
-        var config = Reveal.getConfig();
-        if( !config.dependencies.length ){
-            // user configured no scripts so we can
-            // run our callback immediately
-            c();
-        }else{
-            var dep = config.dependencies[ config.dependencies.length - 1 ];
-            var old_c = dep.callback;
-            dep.callback = function(){
-                if( old_c ){
-                    old_c();
-                }
-                c();
-            }
-        }
+    function configure(o) {
     }
 
-    function configure( o ){
-    }
-
-    function install(){
+    function install() {
         installKeyBindings();
-        installDependencyCallback( function(){
-
-        });
     }
 
     install();
@@ -74,4 +34,4 @@ var Defrag = ( function( Reveal, global ){
         configure: configure
     };
 
-})( Reveal, window );
+})(Reveal, window);
